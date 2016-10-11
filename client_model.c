36dd77b2a8190b2e1d0bdf975e152786010731c6
Mariya <kholmogorovamariya@gmail.com>
2016-10-11 03:07:57 +0300

    The simplest connection.

---
+++ client_model.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+#define MAX_BUF 1024
+
+
+int main(void) {
+    int error_flag, 
+        socket_fd, 
+        client_fd,
+        address_len;
+
+    struct sockaddr_in server_address;
+    char buffer[MAX_BUF];
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        perror("socket");
+        exit(errno);
+    }
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(7000);
+    error_flag = inet_aton("127.0.0.1", 
+            &server_address.sin_addr);
+    if (error_flag == -1) {
+        perror("inet_aton");
+        exit(errno);
+    }
+    address_len = sizeof(struct sockaddr);
+    error_flag = connect(socket_fd, 
+            (struct sockaddr*)&server_address,
+            address_len);
+    if (error_flag == -1) {
+        perror("connect");
+        exit(errno);
+    }
+    strcpy(buffer, "Hello!\n\0");
+    send(socket_fd, buffer, 8, 0);
+    recv(socket_fd, buffer, MAX_BUF, 0);
+    printf("%s", buffer);
+    close(socket_fd);
+    return 0;
+}
---
+++ server_model.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#define MAX_QUEUE 5
+#define MAX_BUF 1024
+
+int make_socket(int port)
+{
+    int socket_fd, error_flag;
+    struct sockaddr_in sock_address;
+
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        perror("socket");
+        exit(errno);
+    }
+    sock_address.sin_family = AF_INET;
+    sock_address.sin_port = htons(port);
+    sock_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    //my_sockaddress.sin_addr.s_addr = INADDR_ANY;
+    error_flag = bind(socket_fd, 
+            (struct sockaddr *)&sock_address,
+            sizeof(struct sockaddr));
+    if (error_flag == -1) {
+        perror("bind");
+        exit(errno);
+    }
+    return socket_fd;
+}
+
+
+int main(void) {
+    int error_flag, 
+        socket_fd, 
+        client_fd,
+        address_len,
+        sent_num;
+    struct sockaddr_in client_address;
+    char buffer[MAX_BUF];
+
+    socket_fd = make_socket(7000);
+    error_flag = listen(socket_fd, MAX_QUEUE);
+    if (error_flag == -1) {
+        perror("listen");
+        exit(errno);
+    }
+    while (1) {
+        client_fd = accept(socket_fd,
+                (struct sockaddr*)&client_address,
+                &address_len);
+        if (client_fd == -1) {
+            perror("accept");
+            exit(errno);
+        }
+        printf("%s : %d connected\n", 
+                inet_ntoa(client_address.sin_addr),
+                ntohs(client_address.sin_port));
+       /* printf("%s  connected\n", 
+                (char *)inet_ntoa(client_address.sin_addr));
+                ntohs(client_address.sin_port));*/
+
+        sent_num = send(client_fd, buffer,
+                recv(client_fd, buffer, MAX_BUF, 0), 0);
+        if (sent_num == -1) {
+            perror("send");
+            exit(errno);
+        }
+        close(client_fd);
+    }
+    close (socket_fd);
+    return 0;
+}
+        
+
+
